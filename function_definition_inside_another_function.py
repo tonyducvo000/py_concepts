2a2dc655b4a7bbb33106a8e8b88163fc697191fc
tony <tonyducvo000@gmail.com>
2021-08-14 18:53:15 -0700

    fix comment

--- function_definition_inside_another_function.py
+++ function_definition_inside_another_function.py
@@ -27,9 +27,9 @@ func = print_msg("Python is awesome")
 #the print statement, print(greeting, message) in printer() function
 #is accessing these variables - this demonstrates closure.
 
-#closure is simply a value that remembers and variables in it's enclosing scope
-#even if the outer function is done executing
+#closure is simply a function that remembers the values and variables in it's enclosing scope
+#even if the outer function is done executing!
 
-#the line below is valid - it is calling the inner print() function
+#the line below is valid - it is actually calling the inner print() function
 func()
 
