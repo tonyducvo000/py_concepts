d626d278a7644231a4b9c5d80307e9e2675f48dc
tony <tonyducvo000@gmail.com>
2021-08-29 14:18:06 -0700

    fixed comments

--- abstract_base_class/abstract_base_class.py
+++ abstract_base_class/abstract_base_class.py
@@ -1,12 +1,19 @@
+##################################################################################################################################################################
+
 # What are abstract classes?
 # Abstract class can be considered as a blueprint for other classes.
 # Abstract classes must contain one or more abstract methods.
 # Abstract methods is a method that is declared but contains no implementation.
 # Abstract classes may not be instantiated, and its abstract methods must be implemented by its subclasses.
 
+# When and how to use abstract classes:
 # Abstract base classes provide an interface and make sure that the derived classes are implemented properly.
+# Abstract base classes provide a way to define interfaces when other techniques like hasattr() would be clumsy or subtly wrong (for example with magic methods).
+# Subclass of abstract classes must implement ALL of the abstract methods.
 
-# Class to demonstrate problem of instantiating base class, and
+##################################################################################################################################################################
+
+# Class to demonstrate problem of instantiating base class, and error when calling an abstract method (i.e., Concrete subclass did NOT override abstract base method).
 class Base:
     def foo(self):
         raise NotImplementedError()
@@ -35,3 +42,4 @@ print(isinstance(conObj, Concrete))  # Prints True
 # This becomes a problem ONLY when user calls the bar() method.
 conObj.bar()
 
+# In abstract_base_class_decorator.py, solution to problem 1 and 2 will be provided.
