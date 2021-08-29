19138061e4db669f4d099c95f1ca88913489b10c
tony <tonyducvo000@gmail.com>
2021-08-29 15:11:07 -0700

    fixes for PEP compliancy

--- abstract_base_class/abstract_base_class_decorator.py
+++ abstract_base_class/abstract_base_class_decorator.py
@@ -1,6 +1,11 @@
 # To remedy the problems presented in abstract_base_class,py, Python's abc module is used.
 
+# How to use abc:
+# Base class subclasses ABCMeta and abstract methods are decorated with @abstractmethod function decorators.
+# Subclass of Base class MUST implement all abstract methods.
+
 from abc import ABCMeta, abstractmethod
+
 
 class Base(metaclass=ABCMeta):
     @abstractmethod
@@ -15,9 +20,11 @@ class Concrete(Base):
     def foo(self):
         pass
 
+
 # Problem #1 solution - @abstractmethod prevents Base class from being instantiated.
 # TypeError: Can't instantiate abstract class Base with abstract methods bar, foo will be thrown.
 baseObj = Base()
+
 
 # Problem #2 solution - @abstract method prevents Concrete from being instantiated
 # Concrete class did not implement all of the methods in Base class.
